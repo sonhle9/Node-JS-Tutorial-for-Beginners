bfab71d61d6bb4037e0c97d383be11b5bd2d90f5
markstoryy <nguyenducmanh13021995@gmail.com>
2022-03-30 17:17:51 +0700

    continue

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Node-JS-Tutorial-for-Beginners
 https://www.youtube.com/watch?v=EprUiOvF3Lo&list=PLC3y8-rFHvwhco_O8PS1iS9xRrdVTvSIz&index=6
+git@github.com:gopinav/Node-JS-Tutorial-for-Beginners.git
--- app.js
+++ app.js
@@ -46,6 +46,70 @@
 //   console.log("Queried the database and delivered data in 5 seconds");
 // }
 //5 modules
-var athletics = require('./athletics');
-athletics.relay();
-athletics.longjump();
+// var athletics = require('./athletics');
+// athletics.relay();
+// athletics.longjump();
+//6 http module
+// var http = require('http');
+// http.createServer(function onRequest(request,response) {
+// 	response.writeHead(200, {"Content-Type": "text/plain"})
+// 	response.write("Hello World");
+// 	response.end();
+// }).listen(8888);
+// Chrome >> http://localhost:8888/ >> Hello World
+//7 events
+// var events = require('events');
+// var eventEmitter = new events.eventEmitter();
+
+// var ringbell = function(){
+//   console.log("Ring ring ring")
+//   // eventEmitter.emit('bellrings','Welcome');
+// }
+
+// eventEmitter.on('doorOpen',ringbell);
+// eventEmitter.on("bellrings", function(message){
+//   console.log(message);
+// })
+// eventEmitter.emit('doorOpen');
+// eventEmitter.emit('bellrings','Welcome');
+//8 fs module FileSystem đọc file
+// var fs = require('fs'); 
+
+// fs.readFile('input.txt', function(err, data){
+//   if(err){
+//     console.log(err);
+//   }else{
+//     console.log("Async data is " + data.toString());
+//   }
+// })
+
+
+// var data = fs.readFileSync('input.txt');
+// console.log("Sync data is " + data.toString());
+// console.log("This is the end");
+// 9 Streams
+// var fs = require('fs');
+
+// var readableStream = fs.createReadStream('input.txt')
+// var data = '';
+// readableStream.setEncoding('UTF8');
+// readableStream.on('data',function(chunk){
+//   data +=chunk;
+// });
+// readableStream.on('end',function(){
+//   console.log(data);
+// });
+
+// var writeData = "Hello World";
+// var writeableStream = fs.createWriteStream('output.txt');
+// writeableStream.write(writeData,'UTF8');
+// writeableStream.end();
+// writeableStream.on('finish', function(){
+//   console.log("Write completed")
+// });
+//10 Pipes
+// var fs = require('fs');
+// var readableStream = fs.createReadStream('in.txt');
+// var writeableStream = fs.createWriteStream('out.txt');
+// readableStream.pipe(writeableStream);
+// 11 - Build your own application
